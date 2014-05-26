bd00107b2ef7f45941be3bbd8dad1275ffa59f6b
Bobby Grayson <bobbygrayson@gmail.com>
2014-05-26 13:55:52 -0400

    add incrementation and decrementation in std lamda calculus form

--- lib/functionalities.rb
+++ lib/functionalities.rb
@@ -1,3 +1,7 @@
-TRUE  = -> x { -> y { x } }
-FALSE = -> x { -> y { y } }
-IF    = -> b { -> x { -> y { b[x][y] } } }
+TRUE    = -> x { -> y { x } }
+FALSE   = -> x { -> y { y } }
+IF      = -> b { b }
+IS_ZERO = -> n { n[-> x { FALSE }][TRUE] }
+INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
+DECREMENT = -> n { -> f { -> x { n[-> g { -> h { h[g[f]] } }]
+                                  [-> y { x }][-> y { y }] } } }
