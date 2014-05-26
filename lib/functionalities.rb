0092177c92b2a61ba7d71dacf9a454c6d272f34f
Bobby Grayson <bobbygrayson@gmail.com>
2014-05-26 13:57:04 -0400

    guardfile update, gemfile fix, and maths functionality

--- lib/functionalities.rb
+++ lib/functionalities.rb
@@ -5,3 +5,7 @@ IS_ZERO = -> n { n[-> x { FALSE }][TRUE] }
 INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
 DECREMENT = -> n { -> f { -> x { n[-> g { -> h { h[g[f]] } }]
                                   [-> y { x }][-> y { y }] } } }
+ADD      = -> m { -> n { n[INCREMENT][m] } }
+SUBTRACT = -> m { -> n { n[DECREMENT][m] } }
+MULTIPLY = -> m { -> n { n[ADD[m]][ZERO] } }
+POWER    = -> m { -> n { n[MULTIPLY[m]][ONE] } }
