1cb7809162a4df661610cf956cf6637c3c6b4ff9
Bobby Grayson <bobbygrayson@gmail.com>
2014-05-26 20:32:02 -0400

    add fold method to resemble inject

--- lib/functionalities.rb
+++ lib/functionalities.rb
@@ -12,6 +12,7 @@ POWER             = -> m { -> n { n[MULTIPLY[m]][ONE] } }
 IS_LESS_OR_EQUAL  = -> m { -> n {
                     IS_ZERO[SUBTRACT[m][n]]
                     } }
+
 MOD =
   Z[-> f { -> m { -> n {
     IF[IS_LESS_OR_EQUAL[n][m]][
@@ -19,8 +20,10 @@ MOD =
         f[SUBTRACT[m][n]][n][x]
         }][m]
         } } }]
+
 Z = -> f { -> x { f[-> y { x[x][y] }] }
           [-> x { f[-> y { x[x][y] }] }] }
+
 PAIR  = -> x { -> y { -> f { f[x][y] } } }
 LEFT  = -> p { p[-> x { -> y { x } } ] }
 RIGHT = -> p { p[-> x { -> y { y } } ] }
@@ -28,6 +31,7 @@ EMPTY     = PAIR[TRUE][TRUE]
 UNSHIFT   = -> l { -> x {
               PAIR[FALSE][PAIR[x][l]]
             } }
+
 IS_EMPTY  = LEFT
 FIRST     = -> l { LEFT[RIGHT[l]] }
 REST      = -> l { RIGHT[RIGHT[l]] }
@@ -35,3 +39,10 @@ RANGE     = Z[-> f {
             -> m { -> n {
               IF[IS_LESS_OR_EQUAL[m][n]][-> x { UNSHIFT[f[INCREMENT[m]][n]][m][x] } ] [EMPTY] } } }]
 
+FOLD      = Z[-> f { -> l { -> x { -> g { 
+              IF[IS_EMPTY[l]][x][-> y 
+                {g[f[REST[l]][x][g]][FIRST[l]][y]}]
+                } 
+              } 
+            }
+          }]
